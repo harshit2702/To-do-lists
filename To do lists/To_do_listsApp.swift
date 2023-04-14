09526eeb7ea84a6eaa37b6f7db9155d4a03dc08e
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-04-14 22:39:12 +0530

    merging error

---
+++ To do lists/To_do_listsApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listsApp.swift
+//  To do lists
+//
+//  Created by Harshit Agarwal on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
