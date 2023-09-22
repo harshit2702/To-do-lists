b1c99bdca6156cd867c8bccc4d57e98d104c57f4
harshitAgarwal <76833764+harshit2702@users.noreply.github.com>
2023-09-22 09:26:54 +0530

    Update SortingFunction.swift

--- To do lists/SortingFunction.swift
+++ To do lists/SortingFunction.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+func sortedItems(_ list: [newLists]) -> [(date: Date, lists: [newLists])] {
+    let groupedItems = Dictionary(grouping: list){list in
+        Calendar.current.startOfDay(for: list.date)
+        
+    }
+    let sortedKeys = groupedItems.keys.sorted(by: >)
+    return sortedKeys.map{key in
+        (date: key,lists: groupedItems[key]!)
+    }
+}
