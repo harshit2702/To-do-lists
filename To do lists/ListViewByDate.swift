5e0160a8f5c3cf22970c6dd31eed4a8316c582b8
harshitAgarwal <76833764+harshit2702@users.noreply.github.com>
2023-09-22 09:26:00 +0530

    Update ListViewByDate.swift

--- To do lists/ListViewByDate.swift
+++ To do lists/ListViewByDate.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct ListViewByDate: View {
+    @Binding var newlists: [newLists]
+    @Binding var isUnlocked: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(sortedItems(newlists), id: \.date){ section in
+                Section(header: Text(section.date, style: .date)){
+                    ForEach(section.lists){ list in
+                        if(isUnlocked || !list.isprivate) {
+                            HStack {
+                                VStack {
+                                    Text(list.name)
+                                }
+                                Spacer()
+                                if(list.isprivate){
+                                    Text("Private")
+                                        .fontWeight(.bold)
+                                }
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+            }
+        }
     }
+    func deleteItems(at offsets: IndexSet) {
+        newlists.remove(atOffsets: offsets)
+        save(newlists)
+        }
 }
 
-#Preview {
-    ListViewByDate()
-}
+//#Preview {
+//    ListViewByDate()
+//}
