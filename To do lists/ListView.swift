9048159dddbf81dd1a03b0df6c1b57df9e060bc7
harshitAgarwal <76833764+harshit2702@users.noreply.github.com>
2023-09-22 09:25:29 +0530

    Update ListView.swift

--- To do lists/ListView.swift
+++ To do lists/ListView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ListView: View {
+    @Binding var newlists: [newLists]
+    @Binding var isUnlocked: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(newlists) { list in
+                if(isUnlocked || !list.isprivate) {
+                    HStack {
+                        if list.tag != newLists.tag.none {
+                            Circle()
+                                .fill(Color(list.tag.rawValue))
+                                .frame(width: 20, height: 20)
+                                }
+                        else{
+                            ZStack{
+                                Circle()
+                                    .frame(width: 20.0)
+                                    .foregroundColor(.black)
+                                Circle()
+                                    .frame(width: 17.0)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        
+            
+                        VStack(alignment: .leading) {
+                            Text(list.name)
+                                .font(.title)
+                                .fontWeight(.medium)
+                            
+                            Text(list.date.formatted(.dateTime
+                                .month(.abbreviated)
+                                .day(.twoDigits)))
+                            .multilineTextAlignment(.leading)
+                            
+                        }
+                        Spacer()
+                        if(list.isprivate){
+                            Text("Private")
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+            }
+            .onDelete(perform: deleteItems)
+        }
     }
+    
+    func deleteItems(at offsets: IndexSet) {
+        newlists.remove(atOffsets: offsets)
+        save(newlists)
+        }
 }
 
-#Preview {
-    ListView()
-}
+//#Preview {
+//    ListView(newlists: <#Binding<[newLists]>#>, isUnlocked: <#Binding<Bool>#>)
+//}
