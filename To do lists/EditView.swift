baf1a69759925a28602bb45a2ca78d1d6483d622
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-09-29 23:19:18 +0530

    fix bug that doesn't show newitem add in ListView , add datepicker and edit view

---
+++ To do lists/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  To do lists
+//
+//  Created by Harshit Agarwal on 29/09/23.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
