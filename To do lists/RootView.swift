d797f379a029a0de28a13103d2ef80b6618b22db
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-10-02 15:05:04 +0530

    security fixes

---
+++ To do lists/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  To do lists
+//
+//  Created by Harshit Agarwal on 02/10/23.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ To do lists/loginView.swift
@@ -0,0 +1,18 @@
+//
+//  loginView.swift
+//  To do lists
+//
+//  Created by Harshit Agarwal on 02/10/23.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginView()
+}
